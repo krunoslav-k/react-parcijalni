a8d8927131929dbc73e078e59f97e879b923ae7e
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2025-07-27 09:12:06 +0200

    style: add layout styling to RepoListComponent and sort repos by starts

--- src/components/RepoListComponent/RepoListComponent.jsx
+++ src/components/RepoListComponent/RepoListComponent.jsx
@@ -5,11 +5,14 @@ export default function RepoListComponent({ repos }) {
   return (
     <div className={styles.reposListWrapper}>
       <ul className={styles.reposList}>
-        {repos.slice(0, 12).map((repo) => (
-          <li key={repo.id} className={styles.repoItem}>
-            {repo.name}
-          </li>
-        ))}
+        {repos
+          .sort((a, b) => b.stargazers_count - a.stargazers_count)
+          .slice(0, 12)
+          .map((repo) => (
+            <li key={repo.id} className={styles.repoItem}>
+              {repo.name}
+            </li>
+          ))}
       </ul>
     </div>
   );
